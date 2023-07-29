42e14f6f0a2553371cf75feca00201efa1c8ecc6
Yulun Zhang <yulunz@andrew.cmu.edu>
2023-07-29 17:16:37 +0800

    add param in plotting meta script

--- scripts/plot_stats_in_dir.sh
+++ scripts/plot_stats_in_dir.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 TO_PLOT="$1"
+ADD_LEGEND="$2"
+
+if [ -z "${ADD_LEGEND}" ]; then
+    ADD_LEGEND="True"
+fi
+
 
 for LOGDIR in ${TO_PLOT}/*;
 do
     printf "Plotting $LOGDIR\n"
     python analysis/plot_stats_vs_n_agents.py \
-        --logdirs $LOGDIR
+        --logdirs $LOGDIR \
+        --add-legend $ADD_LEGEND
     printf "Done!\n\n"
 done
