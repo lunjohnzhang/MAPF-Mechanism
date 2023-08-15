5d7842d759aa324dcd3078991dc3fe14ded33689
Yulun Zhang <yulunz@andrew.cmu.edu>
2023-08-15 13:02:47 +0800

    parameterize gen scen script

--- scripts/gen_scens.sh
+++ scripts/gen_scens.sh
@@ -1,3 +1,7 @@
+MAP_NAME="$1"
+N_AGENTS="$2"
+
+
 for i in $(seq 1 10); do
-    ./build/drone -m maps/Paris_1_256.map -a custom_scens/Paris_1_256-my-$i.scen -t 120  --cost config/agent_costs/uniform/1000_$i.json --value config/agent_values/uniform/1000_$i.json --seed $i -k 20000 --nLayers 1 --algo PP1 --nRuns 1 --screen 1 &
+    ./build/drone -m maps/${MAP_NAME}.map -a custom_scens/${MAP_NAME}-my-$i.scen -t 120  --cost config/agent_costs/uniform/1000_$i.json --value config/agent_values/uniform/1000_$i.json --seed $i -k ${N_AGENTS} --nLayers 1 --algo PP1 --nRuns 1 --screen 1 &
 done
