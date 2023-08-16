447d4e476dbc4bd25d92380a137e36e772f0ec73
Yulun Zhang <yulunz@andrew.cmu.edu>
2023-08-16 16:51:05 +0800

    update payment plotting

--- analysis/all_payments_hist.py
+++ analysis/all_payments_hist.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import argparse
 
+
 def main(dir_path, num_agents, plot_extra_name):
     # Big array to collect all the payments
     payments_array = []
@@ -11,33 +12,44 @@ def main(dir_path, num_agents, plot_extra_name):
     # Looping through subdirectories
     for subdir, _, _ in os.walk(dir_path):
         result_file_path = os.path.join(subdir, 'result.json')
-        
+
         # Check if the result.json file exists
         if os.path.isfile(result_file_path):
             with open(result_file_path, 'r') as file:
                 data = json.load(file)
-                
+
                 # Get the payments
                 payments = data.get('payments', [])
-                
+
                 # Check if the length of payments is equal to num_agents
                 if len(payments) == num_agents:
                     payments_array += payments
 
     # Plotting the histogram
-    plt.hist(payments_array, bins=20, edgecolor='black')
-    plt.title(f'Histogram of Payments')
-    plt.xlabel('Payments')
-    plt.ylabel('Frequency')
-    plt.savefig(Path(args.dir_path)/Path(f'histogram_payments.png'))
+    plt.figure(figsize=(8, 6))
+    plt.hist(payments_array, bins=20, edgecolor='black', log=True)
+    plt.title(f'Histogram of Payments', fontsize=25)
+    plt.xlabel('Payments', fontsize=23)
+    plt.ylabel('Frequency', fontsize=23)
+    plt.tick_params(axis='both', which='major', labelsize=18)
+    plt.tick_params(axis='both', which='minor', labelsize=15)
+    plt.tight_layout()
+    plt.savefig(Path(args.dir_path) / Path(f'histogram_payments.pdf'))
     plt.close()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process a directory and plot payments.')
-    parser.add_argument('dir_path', type=str, help='Path to the directory containing subdirectories')
-    parser.add_argument('num_agents', type=int, help='Number of agents for comparison')
-    parser.add_argument('--plot-extra-name', type=int, help='extra stuff to put in plot')
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Process a directory and plot payments.')
+    parser.add_argument('dir_path',
+                        type=str,
+                        help='Path to the directory containing subdirectories')
+    parser.add_argument('num_agents',
+                        type=int,
+                        help='Number of agents for comparison')
+    parser.add_argument('--plot-extra-name',
+                        type=int,
+                        help='extra stuff to put in plot')
 
     args = parser.parse_args()
     main(args.dir_path, args.num_agents, args.plot_extra_name)
--- analysis/plot_payment.py
+++ analysis/plot_payment.py
@@ -7,9 +7,13 @@ import matplotlib.pyplot as plt
 def plot_payments_histogram(payments, algo_name, save_path):
     plt.figure(figsize=(8, 5.5))
     plt.hist(payments, edgecolor='black', alpha=0.7)
-    plt.title(f'Payments Distribution for {algo_name} Across All Runs', fontsize=15)
-    plt.xlabel('Payment Value', fontsize=12)
-    plt.ylabel('Frequency', fontsize=12)
+    plt.title(f'Payments Distribution for {algo_name} Across All Runs',
+              fontsize=15)
+    plt.xlabel('Payment Value', fontsize=25)
+    plt.ylabel('Frequency', fontsize=25)
+    plt.xlim(0, None)
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    plt.tick_params(axis='both', which='minor', labelsize=15)
     plt.grid(axis='y', alpha=0.75)
     plt.tight_layout()
     plt.savefig(os.path.join(save_path, f'{algo_name}_payments_histogram.png'))
@@ -18,30 +22,43 @@ def plot_payments_histogram(payments, algo_name, save_path):
 
 
 def plot_payment(logdir):
+    with open(os.path.join(logdir, "config.json"), "r") as f:
+        config = json.load(f)
     with open(os.path.join(logdir, "result.json"), "r") as f:
-        data = json.load(f)
+        result = json.load(f)
 
-    payments = data["payments"]
+    current_algo = config["algo"]
+    payments = result["payments"]
 
-    # data to plot
-    epsilon = 1e-7
-    x = [x+epsilon for x in range(len(payments))]
-    y = payments
+    if current_algo == "PP":
+        current_algo = "Monte Carlo PP"
+    elif current_algo == "PP1":
+        current_algo = "First Come First Serve"
+    elif current_algo == "PBS" and config["exhaustiveSearch"]:
+        current_algo = "Exhaustive PBS"
 
-    # create a figure and an Axes object
-    fig, ax = plt.subplots(figsize=(12, 6))
+    plot_payments_histogram(payments, current_algo, logdir)
 
-    # create a bar plot using the Axes object
-    ax.bar(x, y, width=0.6)
+    # # data to plot
+    # epsilon = 1e-7
+    # x = [x+epsilon for x in range(len(payments))]
+    # y = payments
 
-    # set the title and axis labels
-    ax.set_title("Payments of Agents", fontsize=30)
-    ax.set_xlabel("Agent ID", fontsize=20)
-    ax.set_ylabel("Payment", fontsize=20)
-    ax.set_ylim(-5, 5)
+    # # create a figure and an Axes object
+    # fig, ax = plt.subplots(figsize=(12, 6))
 
-    # save the plot as a PNG image file
-    fig.savefig(os.path.join(logdir, "payment.png"))
+    # # create a bar plot using the Axes object
+    # ax.bar(x, y, width=0.6)
+
+    # # set the title and axis labels
+    # ax.set_title("Payments of Agents", fontsize=30)
+    # ax.set_xlabel("Agent ID", fontsize=20)
+    # ax.set_ylabel("Payment", fontsize=20)
+    # ax.set_ylim(-5, 5)
+
+    # # save the plot as a PNG image file
+    # fig.savefig(os.path.join(logdir, "payment.png"))
+
 
 if __name__ == "__main__":
     fire.Fire(plot_payment)
