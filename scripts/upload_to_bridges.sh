40ef7cb4994868083439da09dea2f83246fde592
Yulun Zhang <yulunzha@usc.edu>
2024-01-25 01:15:22 -0500

    remove upload download script

--- scripts/upload_to_bridges.sh
+++
@@ -1,6 +0,0 @@
-TO_UPLOAD="$1"
-
-# zip the directory
-zip -qr - $TO_UPLOAD | pv -bep -s $(du -bs $TO_UPLOAD | awk '{print $1}') > $TO_UPLOAD.zip
-
-scp $TO_UPLOAD.zip zhange@bridges2.psc.edu:/ocean/projects/cis220074p/zhange/logs
